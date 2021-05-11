33e1af26da79149a0eed0b7f675dd0175bd3bf86
bwcsemaj <bwc_semaj@live.com>
2021-05-10 23:47:07 -0400

    removed comparator class in solution

---
+++ client/src/main/java/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+        import java.util.Scanner;
+
+// Write your code here. DO NOT use an access modifier in your class declaration.
+class Solution {
+    public static void main(String[] args) {
+        
+        int i = 010;
+        int j = 07;
+        System.out.println(i);
+        System.out.println(j);
+        /* /* */
+    }
+    
+}
